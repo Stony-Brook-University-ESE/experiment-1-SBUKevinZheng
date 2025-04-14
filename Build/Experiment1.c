99d8c6e05c729e6c0835893621cbe07c158d8b22
Quickmathisgood <quickmathisgood@gmail.com>
2025-04-14 13:27:19 -0400

    Completed

--- Build/Experiment1.c
+++ Build/Experiment1.c
@@ -2,10 +2,22 @@
 #include <string.h>
 
 int main() {
-    char str[8];
+    char str[20];
     printf("Enter a word: ");
     scanf("%s", str);
-
+    
+    char a;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        a = str[i];
+        if (a>='a'&&a<='z')
+        {
+            a = a - 32;
+            str[i] = a;
+        }
+        
+    }
+    
     int len = strlen(str);
     int isPalindrome = 1;
 
